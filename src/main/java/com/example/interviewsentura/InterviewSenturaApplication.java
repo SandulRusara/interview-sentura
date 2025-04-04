8967898460a0eaae699da803bd63319a8d1d7267
SandulRusara <sandulrusara8@gmail.com>
2025-04-04 08:55:06 +0530

    first commit

---
+++ src/main/java/com/example/interviewsentura/InterviewSenturaApplication.java
@@ -0,0 +1,13 @@
+package com.example.interviewsentura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewSenturaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewSenturaApplication.class, args);
+    }
+
+}
